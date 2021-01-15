acbac9f481744497cc0aee56f11b64a7f21d4f2f
Efim Zabarsky <zzfima@gmail.com>
2021-01-15 17:36:57 +0200

    see some data

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
-courseDF = pd.read_excel('RC_F01_01_2017_T01_01_2020.xlsx')
+courseDF = pd.DataFrame(pd.read_excel('RC_F01_01_2017_T01_01_2020.xlsx'))
 print(courseDF.head())
+print(courseDF.describe())
+print(courseDF.info())
+courseDF['curs'].hist()
+plt.show()
